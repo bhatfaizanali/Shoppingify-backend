3c69ed54781410b65bd4a9ad29b7603bcf458ce3
Faizan Bhat <faizan.bhat@mountblue.tech>
2020-11-10 16:03:11 +0530

    Removed expiry for access token

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -14,7 +14,7 @@ const schema = Joi.object({
 });
 
 function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 }
 
 exports.signIn = async (request, response) => {
